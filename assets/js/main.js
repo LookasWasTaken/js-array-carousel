6931dc4520e1030b2b0dd1230929e1f282906363
LookasWasTaken~ <piacen.luca@gmail.com>
2023-03-14 16:04:16 +0100

    add fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 // array
 
 const images = [
-  "../assets/img/01.webp",
-  "../assets/img/02.webp",
-  "../assets/img/03.webp",
-  "../assets/img/04.webp",
-  "../assets/img/05.webp",
+  "./assets/img/01.webp",
+  "./assets/img/02.webp",
+  "./assets/img/03.webp",
+  "./assets/img/04.webp",
+  "./assets/img/05.webp",
 ];
 
 // image selector
